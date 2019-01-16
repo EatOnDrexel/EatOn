4f3c8d686b5a88200a67cf4775f1f16e4b34d66f
James Hemmen <Bobertbarker101@gmail.com>
2019-01-16 16:27:54 -0500

    Added nutrient variables

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,12 @@
 #lorem
 
-dailycalories = int(input("What is your daily caloric limit?"))
+Calories_Consumed = int(input("What is your daily caloric limit?"))
+
+Protein_Consumed = int(input("How many grams of protein have you consumed today?"))
+Fat_Consumed = int(input("How many grams of fat have you consumed today?"))
+Carbs_Consumed = int(input("How many grams of carbohydrates have you consumed today?"))
+
+Calories_From_Protein = Protein_Consumed * 4
+Calories_From_Fat = Fat_Consumed * 4
+Calories_From_Carbs = Carbs_Consumed * 9
+
