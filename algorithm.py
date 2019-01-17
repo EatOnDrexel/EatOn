bdced46bd9e3ef5904f8c3442e43502ad84fa3d2
James Hemmen <Bobertbarker101@gmail.com>
2019-01-16 20:51:13 -0500

    Added ability to calculate remaining grams to eat of each macro

--- algorithm.py
+++ algorithm.py
@@ -12,30 +12,70 @@ Carbs_Consumed = int(input("How many grams of carbohydrates have you consumed to
 
 
 # Calculating how many calories of each macro have been eaten that day
-Calories_From_Protein = Protein_Consumed * 4
-Calories_From_Fat = Fat_Consumed * 4
-Calories_From_Carbs = Carbs_Consumed * 9
+Protein_Consumed_Calories = Protein_Consumed * 4
+Fat_Consumed_Calories = Fat_Consumed * 9
+Carbs_Consumed_Calories = Carbs_Consumed * 4
 
 # Calculate how many calories have been consumed and how many are left for the day
-Calories_Consumed = Calories_From_Protein + Calories_From_Fat + Calories_From_Carbs
+Calories_Consumed = Protein_Consumed_Calories + Fat_Consumed_Calories + Carbs_Consumed_Calories
 Calories_Remaining = Calories_Limit - Calories_Consumed
 
-print(Calories_From_Protein)
-print(Calories_From_Fat)
-print(Calories_From_Carbs)
+print(Protein_Consumed_Calories)
+print(Fat_Consumed_Calories)
+print(Carbs_Consumed_Calories)
 
-# Diets stored in arrays with percentage breakdown of protein, fat, & carbs
 
-Diet_Keto = [30, 60, 10]
-Diet2 = [0, 0, 0]
-Diet3 = [0, 0, 0]
-Diet4 = [0, 0, 0]
+print("What percentage of your diet should be made up of protein?")
+Protein_Desired = (int(input()) / 100)
 
-Diet_Custom = [0, 0, 0]
+print("What percentage of your diet should be made up of fat")
+Fat_Desired = (int(input()) / 100)
 
-# print("Please Choose the Diet You Are Following.")
-# print("The numbers shown represent the percentage breakdown of protein, fat, and carbs.")
-# print("\n")
-# print("Keto: " + Diet_Keto)
+print("What percentage of your diet should be made up of carbs?")
+Carbs_Desired = (int(input()) / 100)
 
-Test
+
+# Calculate how many calories the user has consumed so far that day
+Total_Calories_So_Far = Protein_Consumed_Calories + Fat_Consumed_Calories + Carbs_Consumed_Calories
+
+# Calculate the percentage of calories from each macro
+if Total_Calories_So_Far != 0:
+    Percent_From_Protein = Protein_Consumed_Calories / Total_Calories_So_Far
+    Percent_From_Fat = Fat_Consumed_Calories / Total_Calories_So_Far
+    Percent_From_Carbs = Carbs_Consumed_Calories / Total_Calories_So_Far
+
+else:
+    Percent_From_Protein = 0
+    Percent_From_Fat = 0
+    Percent_From_Carbs = 0
+
+Diet_Current = [Percent_From_Protein, Percent_From_Fat, Percent_From_Carbs]
+
+# Calculate how many grams the user should have of each macro
+Protein_Limit = (Calories_Limit * Protein_Desired) / 4
+Fat_Limit = (Calories_Limit * Fat_Desired) / 9
+Carbs_Limit = (Calories_Limit * Carbs_Desired) / 4
+
+Protein_Remaining_Grams = Protein_Limit - Protein_Consumed
+Fat_Remaining_Grams = Fat_Limit - Fat_Consumed
+Carbs_Remaining_Grams = Carbs_Limit - Carbs_Consumed
+
+print(Protein_Remaining_Grams)
+print(Fat_Remaining_Grams)
+print(Carbs_Remaining_Grams)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
