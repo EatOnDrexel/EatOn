9f81150e442eb456cf5f8ec44b3a2ff273f6205d
James Hemmen <Bobertbarker101@gmail.com>
2019-01-30 22:06:20 -0500

    Finished API call. Moved more features over to functions.py. Added config file for storing API key. Added ability to enter search terms.

--- eaton.py
+++ eaton.py
@@ -3,68 +3,23 @@ import functions
 # EatOn Demo Alpha
 # Author: James Hemmen
 # Drexel University
-# Python 2.7
+# Python 3.6
 
-
-app_id = "lorem"
-app_key = "ipsum"
-number_of_results = 10
-search_term = "chicken"
 
 # Taking initial user inputs
-Calories_Limit = functions.take_calories_input()
+functions.take_calories_input()
 functions.collect_macro_input()
 
+# Calculate how many calories have been consumed and how many grams are left for the day of each macro
+functions.calculate_remaining_calories()
+functions.calculate_remaining_grams()
+functions.calculate_limits()
 
-# Calculate how many calories have been consumed and how many are left for the day
-Calories_Consumed = sum(functions.calculate_calories())
-Calories_Remaining = Calories_Limit - Calories_Consumed
+# Generate API call
+request = functions.generate_API_call()
 
-print("How Many Grams of Each Macro Consumed")
-print(functions.pro_consumed)
-print(functions.fat_consumed)
-print(functions.carbs_consumed)
+print(request)
 
+data = request.json()
 
-Protein_Desired = functions.take_percentages("protein")
-Fat_Desired = functions.take_percentages("fat")
-Carbs_Desired = functions.take_percentages("carbs")
-
-print("Desired Grams of Each Macro")
-print(Protein_Desired)
-print(Fat_Desired)
-print(Carbs_Desired)
-
-
-# Calculate how many grams the user should have of each macro
-Protein_Limit = (Calories_Limit * Protein_Desired) / 4
-Fat_Limit = (Calories_Limit * Fat_Desired) / 9
-Carbs_Limit = (Calories_Limit * Carbs_Desired) / 4
-
-print("Daily Limit for Each Macro in Grams")
-print(Protein_Limit)
-print(Fat_Limit)
-print(Carbs_Limit)
-
-Protein_Remaining_Grams = Protein_Limit - functions.pro_consumed
-Fat_Remaining_Grams = Fat_Limit - functions.fat_consumed
-Carbs_Remaining_Grams = Carbs_Limit - functions.carbs_consumed
-
-# Generating call request
-# THIS IS UNFINISHED
-API_Call = "https://api.edamam.com/search?" + search_term + "&app_id=" + app_id + "&app_key=" + app_key + "&from=0&to=" + str(number_of_results - 1) + "&"
-
-print(API_Call)
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(request.text)
--- functions.py
+++ functions.py
@@ -1,20 +1,39 @@
+import config
+import requests
 
+# Declaring Variables for use later
 pro_consumed = 0
 fat_consumed = 0
 carbs_consumed = 0
 
+Calories_Limit = 0
+
+Protein_Desired = 0
+Fat_Desired = 0
+Carbs_Desired = 0
+
+Protein_Limit = 0
+Fat_Limit = 0
+Carbs_Limit = 0
+
+r = None
+
+
+# Takes user input for how many calories they can have in a day
 def take_calories_input():
     print("What is your daily caloric limit?")
     user_input = int(input())
     return user_input
 
 
+# Takes user input as integer to store as value of a given macro
 def take_input(macro):
     print("How many grams of " + macro + " have you consumed today?")
     user_input = int(input())
     return user_input
 
 
+# Takes input of macros consumed in the day. Runs function take_input for each macro
 def collect_macro_input():
     global pro_consumed
     global fat_consumed
@@ -25,8 +44,8 @@ def collect_macro_input():
     carbs_consumed = take_input("carbs")
 
 
+# Calculating how many calories of each macro have been eaten that day
 def calculate_calories():
-    # Calculating how many calories of each macro have been eaten that day
 
     global pro_consumed
     global fat_consumed
@@ -44,3 +63,61 @@ def take_percentages(macro):
     percent = user_input / 100.0
     return percent
 
+
+# Generating call request
+def generate_API_call():
+    URL = "https://api.edamam.com/search"
+    PARAMS = (
+        ('q', take_search_term()),
+        ('app_id', config.app_id),
+        ('app_key', config.api_key),
+        ('from', '0'),
+        ('to', str(config.number_of_results)),
+        ('nutrients[PROCNT]', str(int(Protein_Remaining_Grams * .9)) + "-" + str(int(Protein_Remaining_Grams))),
+        ('nutrients[FAT]', str(int(Fat_Remaining_Grams * .9)) + "-" + str(int(Fat_Remaining_Grams))),
+        ('nutrients[CHOCDF]', str(int(Carbs_Remaining_Grams * .9)) + "-" + str(int(Carbs_Remaining_Grams))),
+    )
+
+    r = requests.get(url=URL, params=PARAMS)
+    return r
+
+
+# Calculate how many grams the user should have of each macro
+def calculate_limits():
+
+    global Calories_Limit
+    global Protein_Limit
+    global Fat_Limit
+    global Carbs_Limit
+
+    Protein_Limit = (Calories_Limit * take_percentages("protein")) / 4
+    Fat_Limit = (Calories_Limit * take_percentages("fat")) / 9
+    Carbs_Limit = (Calories_Limit * take_percentages("carbs")) / 4
+
+
+# Calculate how many grams the user has left of each macro
+def calculate_remaining_grams():
+
+    global Protein_Remaining_Grams
+    global Fat_Remaining_Grams
+    global Carbs_Remaining_Grams
+
+    Protein_Remaining_Grams = Protein_Limit - pro_consumed
+    Fat_Remaining_Grams = Fat_Limit - fat_consumed
+    Carbs_Remaining_Grams = Carbs_Limit - carbs_consumed
+
+
+def calculate_remaining_calories():
+
+    global Calories_Consumed
+    global Calories_Remaining
+
+    Calories_Consumed = sum(calculate_calories())
+    Calories_Remaining = Calories_Limit - Calories_Consumed
+
+
+# Asks user for search query
+def take_search_term():
+    print("What type of food are you looking for?")
+    user_input = input()
+    return user_input
