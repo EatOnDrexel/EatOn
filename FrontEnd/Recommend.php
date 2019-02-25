4c025444ea517f493fb2da261701ee462a25de24
preechaya-s <46584631+preechaya-s@users.noreply.github.com>
2019-02-24 19:38:11 -0500

    Update Recommend.php

--- FrontEnd/Recommend.php
+++ FrontEnd/Recommend.php
@@ -5,43 +5,18 @@
 	$Pro_Limit = $_POST['RPro'];
 	$Fat_Limit = $_POST['RFat'];
 	
-	$Cal_Consumed = $_POST['CCal'];
 	$Carb_Consumed = $_POST['CCarb'];
 	$Pro_Consumed = $_POST['CPro'];
 	$Fat_Consumed = $_POST['CFat'];
 	
-	//do the calculations for the leftover macros and cals here
-	$Cal = $Cal_Limit - $Cal_Consumed;
-	$Carb = $Carb_Limit - $Carb_Consumed;
-	$Pro = $Pro_Limit - $Pro_Consumed;
-	$Fat = $Fat_Limit - $Fat_Consumed;
-	
-	//check that no calculated value is less than 0, if true, change to 0
-	if($Cal < 0)
-	{
-		$Cal = 0; 
-	}
-	
-	if($Carb < 0)
-	{
-		$Carb = 0;
-	}
-	
-	if($Pro < 0)
-	{
-		$Pro = 0;
-	}
-	
-	if($Fat < 0)
-	{
-		$Fat = 0;
-	}
+	$Search_Term = $_POST['Term'];
 	
 	//send to python script and get returned JSON values
 	//$output should contain the returned value from the python script
-	exec("python newFunctions.py $Pro $Fat $Carb $Cal", $output);
+	//exec("python newFunctions.py $Pro $Fat $Carb $Cal", $output); //this is the old one
+	exec("python newFunctions.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $Search_Term", $output);
 	
-	//parse through returned json values
+	//parse returned json values
 	print($output); //this here for now so that we know it works and return json values
 	
 	//print out in html format
