5368f91314dc797ce2ccbcd5053124ba64c308a1
James Hemmen <Bobertbarker101@gmail.com>
2019-03-04 14:41:50 -0500

    Added path to python call

--- html/Recommend.php
+++ html/Recommend.php
@@ -4,22 +4,22 @@
 	$Carb_Limit = $_POST['RCarb'];
 	$Pro_Limit = $_POST['RPro'];
 	$Fat_Limit = $_POST['RFat'];
-	
+
 	$Carb_Consumed = $_POST['CCarb'];
 	$Pro_Consumed = $_POST['CPro'];
 	$Fat_Consumed = $_POST['CFat'];
-	
+
 	$Search_Term = $_POST['Term'];
-	
+
 	//send to python script and get returned JSON values
 	//$output should contain the returned value from the python script
 	//exec("python newFunctions.py $Pro $Fat $Carb $Cal", $output); //this is the old one
 
-	exec("python GenerateCall.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $Search_Term", $output);
-	
+	exec("python /var/www/eaton/BackEnd/GenerateCall.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $Search_Term", $output);
+
 	//parse returned json values
 	print($output); //this here for now so that we know it works and return json values
-	
+
 	//print out in html format
 	//to be determined: what to display per recipe
 	print('
