45c5abb85b43e17df2f0ce4ed832c467fdef631e
James Hemmen <Bobertbarker101@gmail.com>
2019-03-04 23:54:00 +0000

    Changes to Recommend.php

--- html/Recommend.php
+++ html/Recommend.php
@@ -15,11 +15,17 @@
 	//$output should contain the returned value from the python script
 	//exec("python newFunctions.py $Pro $Fat $Carb $Cal", $output); //this is the old one
 
+	//exec("sudo python3 /var/www/eaton/BackEnd/GenerateCall.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $Search_Term", $output);
 	exec("sudo python3 /var/www/eaton/BackEnd/GenerateCall.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $Search_Term", $output);
+	echo "sudo python3 /var/www/eaton/BackEnd/GenerateCall.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $Search_Term";
+	echo "<br>";
 
 	//parse returned json values
-	print($output); //this here for now so that we know it works and return json values
-
+	//var_dump($output);
+	//var_dump($return);
+	//print_r($output."<br>"); //this here for now so that we know it works and return json values
+        var_dump($output);
+	
 	//print out in html format
 	//to be determined: what to display per recipe
 	print('
