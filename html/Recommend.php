1b04ea7a45d2e27ef0a70c03c0a977e1bb84e0f2
James Hemmen <Bobertbarker101@gmail.com>
2019-03-18 02:49:50 +0000

    Minnie changes

--- html/Recommend.php
+++ html/Recommend.php
@@ -98,14 +98,35 @@
 		//echo '<b>Recipe: </b><a href="'.$recipe['label'].'">'.$recipe['label'].'</a>';
 		echo "<a href='".$recipe['shareAs']."'><img src='".$recipe['image']."' alt='Recipe_Img'></a><br>";
 		//echo "<b>Link: </b>{$recipe['shareAs']}<br>";
-		
-		echo "&emsp;<b>Nutrients: </b><br>";
+		echo "&emsp;<b>Servings: </b>{$recipe['yield']}<br>";
+		echo "&emsp;<b>Total Nutrients: </b><br>";
 		foreach ($recipe['totalNutrients'] as $nutrient)
 		{
 			if ($nutrient['label'] === "Fat" || $nutrient['label'] === "Carbs" || $nutrient['label'] === "Protein")
 			{
 				echo "&emsp;&emsp;" . $nutrient['label'] . ": " . $nutrient['quantity'] . $nutrient['unit'] . "<br>";
 			}
+		}
+		echo "&emsp;&emsp;Calories: " . $recipe['calories'] . "kcal<br>";
+		echo "&emsp;<b>Nutrients per serving: </b><br>";
+		foreach ($recipe['totalNutrients'] as $nutrient)
+		{
+			if ($nutrient['label'] === "Fat" || $nutrient['label'] === "Carbs" || $nutrient['label'] === "Protein")
+			{
+				if ($recipe['yield'] > 1)
+				{
+					$div = floatval($nutrient['quantity'])/intval($recipe['yield']);
+				}
+				echo "&emsp;&emsp;" . $nutrient['label'] . ": " . $div . $nutrient['unit'] . "<br>";
+			}
+		}
+		if ($recipe['yield'] > 1)
+		{
+			echo "&emsp;&emsp;Calories: " . (floatval($recipe['calories'])/intval($recipe['yield'])) . "kcal<br>";
+		}
+		else
+		{
+			echo "&emsp;&emsp;Calories: " . $recipe['calories'] . "kcal<br>";
 		}
 		//echo NL . NL;
 		echo "<br><br>";
