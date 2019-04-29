51f2cd3928eb439af7d5842d8722e64a52eb27d2
James Hemmen <Bobertbarker101@gmail.com>
2019-04-29 12:09:49 -0400

    Update Recommend.php

--- html/Recommend.php
+++ html/Recommend.php
@@ -54,7 +54,7 @@
 	echo "</style>";
 	echo "</head>";
 	echo "<body>";
-	echo "<img src=\"..\eatONbanner.png\">";
+	echo "<img src=\"..\EatONbanner.png\">";
 	function generatevars()
 	{
 		global $recipesData;
