9e1ba2c2d9a4038ffa814c79ccc8ad970d600d18
James Hemmen <Bobertbarker101@gmail.com>
2019-04-29 12:18:51 -0400

    Update Recommend.php

--- html/Recommend.php
+++ html/Recommend.php
@@ -52,7 +52,6 @@
 		echo "}";
 
 	echo ".banner{";
-		echo "display: relative;";
 		echo "align: center;";
 		echo "width: 80%;";
 		echo "height: auto;";
@@ -61,9 +60,9 @@
 	echo "</style>";
 	echo "</head>";
 	echo "<body>";
-	echo "<div class=\"banner\"";
-	echo "<img src=\"..\EatONbanner.png\"  width=\"100%\">";
-	echo "</div>";
+
+	echo "<img src=\"..\EatONbanner.png\"  class=\"banner\">";
+
 	function generatevars()
 	{
 		global $recipesData;
