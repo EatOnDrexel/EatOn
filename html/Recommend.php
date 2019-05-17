48d4943196411898737e4a32dee89e8152002df3
James Hemmen <Bobertbarker101@gmail.com>
2019-05-17 16:02:40 -0400

    Update Recommend.php

--- html/Recommend.php
+++ html/Recommend.php
@@ -20,7 +20,7 @@
 
 
 	//send to python script and get returned JSON values
-	if (empty($Search_Term)):
+	if (empty($Search_Term))
 	{
 		$returned = exec("python3 /var/www/eaton/GenerateCall.py $Pro_Limit $Fat_Limit $Carb_Limit $Cal_Limit $Pro_Consumed $Fat_Consumed $Carb_Consumed $NullSearch");
 	}
